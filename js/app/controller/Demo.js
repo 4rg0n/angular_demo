3b132e54fe1e39c04c8928e4c028d4d6b0ae4472
4rg0n <argonthechecker@gmail.com>
2013-10-10 11:04:09 +0200

    Weitere Beispiele hinzugefügt, HTML Struktur mit Bootstrap erweitert

--- js/app/controller/Demo.js
+++ js/app/controller/Demo.js
@@ -1,18 +1,28 @@
 demoApp.controller('DemoCtrl', function($scope) {
     
     $scope.info = "Wenn du das liest, bist du behindert!!";
-    
-    
+       
     $scope.alertButton = "Gratis!";
     
-    //Alert =D
+    $scope.users = [
+        {"name": "Du", alter: 20},
+        {"name": "bist", alter: 21},    
+        {"name": "behindert!", alter: 25}     
+    ];
+    
+    //Alert 
     $scope.alert = function() {
         $scope.alertTitle = 'BEHINDERT!';
         $scope.alertText = 'Jetzt hast du die Bestätigung!';
         
-        $('.alert').show();
+        $scope.show = true;
     };
     
+    $scope.toggled = true;
     
+    //Toggle
+    $scope.toggle = function() {
+        $scope.toggled = !$scope.toggled;
+    }
     
 });
